f75f9b9693ee784eaf16b3dd8ff907fb40d08607
notoncebut2x <rpnealon@gmail.com>
2016-03-19 11:55:20 -0400

    remote on github looked weird

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@
 $(document).ready(function() {
 
 	var canvas = d3.select("body").append("svg")
-		.attr("width", 760)
-		.attr("height", 700)
+		.attr("width", 900)
+		.attr("height", 500)
 
 	d3.json("../data/ukrNatEarth110m.json", function(data){
 		var group = canvas.selectAll("g")
@@ -11,7 +11,7 @@ $(document).ready(function() {
 			.enter()
 			.append("g")
 
-	var projection = d3.geo.mercator().scale(150,000);
+	var projection = d3.geo.mercator().scale(175,000);
 		var path = d3.geo.path().projection(projection)
 		var areas = group.append("path")
 			.attr("d", path)
