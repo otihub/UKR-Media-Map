7bb5d8e33b69d4e85768e8d7c82fee0f428fa93c
David Saeger <saegeritup@gmail.com>
2017-04-10 10:18:52 -0400

    adds tour.js

---
+++ js/tour.js
@@ -0,0 +1,18 @@
+var tour = new Tour({
+  steps: [
+  {
+    element: "#first",
+    title: "Title Description",
+    content: "Here you will see the geography in your current filter and have the option to clear all filters"
+  },
+  {
+    element: "#map",
+    title: "This is the map!",
+    content: "Data was collected in the highlighted oblasks. More effort was put into collecting around the cities highlighted in red. Click on any of these to filter on geography."
+  },
+  {
+    element: "#map",
+    title: "This is the map!",
+    content: "Data was collected in the highlighted oblasks. More effort was put into collecting around the cities highlighted in red. Click on any of these to filter on geography."
+  },
+]});
