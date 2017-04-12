269dd3f14cc6396d280008cb678419eeca2b7bbc
David Saeger <saegeritup@gmail.com>
2017-04-12 09:46:15 -0400

    changes to tour

--- js/tour.js
+++ js/tour.js
@@ -3,18 +3,12 @@ var tour = new Tour({
   steps: [
   {
     title: "Hello World",
-    content: "From November 27-December 25, 2015 and again from x to y surveys were held to study media preferences and political orientations in the South and East of Ukraine. About 1500 face-to-face interviews and 400 computer-assisted telephone interviews were held.",
+    content: "From November 27-December 25, 2015 and again from x to y surveys were held to study media preferences and political orientations in South and East Ukraine. This interactive interface will give you access to the results of these surveying efforts.",
     orphan: true
   },
   {
-    element: "#first",
-    placement: "left",
-    title: "Overview",
-    content: "View the name of the area you have filtered on. Click the 'reset filters' option to clear all."
-  },
-  {
     element: "#map",
-    title: "The Mapz",
+    title: "The Map",
     backdrop:false,
     content: "Data was collected in the highlighted oblasks. More effort was put into collecting around the cities highlighted in red. Click on any of these to filter on geography.",
     onShown: function(){
@@ -24,14 +18,28 @@ var tour = new Tour({
 
   },
   {
+    element: "#first",
+    placement: "left",
+    title: "Overview",
+    content: "View the name of the area you have filtered on. Click the 'reset filters' option to clear all."
+  },
+  {
     element: "#radioTree",
     title: "Tree Maps",
-    backdrop:false,
-    content: "These are called tree maps, they showcase the top 10 media preferences within the set of data you have filtered. The size of the square shows the relative proportion of that media source amongst the top ten. The percentages displayed represent that sources share of expressed media preferences",
+    content: "These are called tree maps, they showcase the top 10 media preferences. The size of each square shows the relative proportion of that media outlet amongst the top ten. The percentages displayed represent the percentage of all filtered respondants who chose this outlet as their favorite.",
     onShown: function(){
       $(".tour-backdrop").appendTo("#radioTree");
       $(".tour-step-background").appendTo("#radioTree");
-    }
+    	}
+	},
+    {
+      element: "#radioBarChart",
+      title: "Bar Charts",
+      content: "You can filter the data based on any of the categories in these barcharts by clicking on its row!",
+      onShown: function(){
+        $(".tour-backdrop").appendTo("#radioBarChart");
+        $(".tour-step-background").appendTo("#radioBarChart");
+      }
   }
 
 ]});
